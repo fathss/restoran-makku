463cc8088fa7a9e36030ee615d3f756c786af054
fathss <alwifathu@gmail.com>
2025-11-17 14:03:14 +0700

    Add dashboard adminlte

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard Admin')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Dashboard Admin</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="mb-0">Selamat datang di halaman dashboard AdminLTE Anda!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
