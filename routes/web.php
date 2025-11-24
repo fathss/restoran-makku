13a402c6a9baaff540fc7faea60cb39b6fadbd37
faranaan <aan21102006@gmail.com>
2025-11-24 10:55:49 +0700

    web update

--- routes/web.php
+++ routes/web.php
@@ -77,3 +77,50 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+// tanpa login
+// Halaman Utama (Landing Page)
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Halaman Menu & Search
+Route::get('/menu', [MenuController::class, 'index'])->name('menu.index');
+
+
+// ketika login
+Route::middleware('auth')->group(function () {
+    
+
+    //Fitur Cart
+    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+    Route::post('/cart/add', [CartController::class, 'addToCart'])->name('cart.add');
+
+    //Fitur Reservasi
+    Route::get('/reservation', [ReservationController::class, 'create'])->name('reservation.create');
+    Route::post('/reservation', [ReservationController::class, 'store'])->name('reservation.store');
+
+    //Fitur Profil
+    //Lihat Profil
+    Route::get('/my-profile', [ProfileController::class, 'show'])->name('profile.show');
+    
+    //Edit Profil & Password 
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    //Route Hapus Item
+    Route::delete('/cart/remove', [CartController::class, 'remove'])->name('cart.remove');
+
+    //Route Checkout
+    Route::post('/checkout', [App\Http\Controllers\OrderController::class, 'store'])->name('checkout.store');
+
+    //Profil - Riwayat Pesanan
+    Route::get('/my-history', [App\Http\Controllers\ProfileController::class, 'history'])->name('profile.history');
+
+    //password
+    Route::put('/user/password', [App\Http\Controllers\ProfileController::class, 'updatePassword'])->name('user-password.update');
+
+    // Route untuk Update Quantity
+    Route::patch('/cart/update', [CartController::class, 'updateQuantity'])->name('cart.update');
+});
+
+require __DIR__.'/auth.php';
